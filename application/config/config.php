41fb9cc46a421fa9a060255fa5fc17ee45cce8ff
Katie <x2015jfw@stfx.ca>
2018-03-19 09:24:28 -0300

    start

--- application/config/config.php
+++ application/config/config.php
@@ -17,13 +17,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | your installation, but due to security concerns the hostname will be set
 | to $_SERVER['SERVER_ADDR'] if available, or localhost otherwise.
 | The auto-detection mechanism exists only for convenience during
-| development and MUST NOT be used in production!
+| development and MUST NOT be used in production! 
 |
 | If you need to allow multiple domains, remember that this file is still
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.64.2/ci/';
+$config['base_url'] = 'http://192.168.64.2/ci';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://192.168.64.2/ci/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
