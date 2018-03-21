66cbd5c9b059a85b7f1adaf107cf116b13894f0e
DylanCoakley <dylan.coakley11@gmail.com>
2018-03-20 22:12:45 -0300

    Added home page, models, controllers

---
+++ application/views/templates/header.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>URMS</title>
+		<link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+	</head>
+	<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="<?php echo base_url(); ?>">URMS</a>
+			</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>about">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
