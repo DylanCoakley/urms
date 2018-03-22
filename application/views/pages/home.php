9e0421babfb7c7239755f9ddfb910b90bd1ae930
Katie <x2015jfw@stfx.ca>
2018-03-22 11:26:58 -0300

    Old stuff

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,3 +1,2 @@
 <h2>Home</h2>
 <h3 class="title-menu">Component List</h3>
-
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,8 @@
 <html>
 	<link href="<?php echo base_url().'/assets/css/bootstrap.min.css';?>" rel="stylesheet">
 
+
+
  	<link href="<?php echo base_url().'/assets/css/jasny-bootstrap.min.css';?>" rel="stylesheet">
 
  	<link href="<?php echo base_url().'/assets/css/main.css';?>" rel="stylesheet">
@@ -13,50 +15,7 @@
 
   <link href="<?php echo base_url().'/assets/css/custom.css';?>" rel="stylesheet">
 
-<<<<<<< HEAD
  	
-=======
-	<head>
-		<div id="headers">
-        <!-- Light header -->
-        <header id="header-style-1">
-          <nav class="navbar navbar-toggleable-sm navbar-light bg-faded">
-            <div class="container">
-              <a class="navbar-brand" href="index.html"><img src="<?php echo base_url(); ?>assets/img/logo-2.png" alt=""></a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i>
-              </button>
-              <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link" href="index.html">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="services.html">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="sliders.html">Team</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Login/SignUp</a>
-                    <div class="dropdown-menu">
-                      <a class="dropdown-item" href="<?php echo base_url(); ?>users/login">Login</a>
-                      <a class="dropdown-item" href="<?php echo base_url(); ?>users/register">Sign Up</a>
-                    </div>
-                    <!--
-                    <?php if(!$this->session->userdata('logged_in')) : ?>
-                      <li><a href="<?php echo base_url(); ?>users/login">Login</a></li>
-                      <li><a href="<?php echo base_url(); ?>users/register">Register</a></li>
-                    <?php endif; ?>
-                    <?php if($this->session->userdata('logged_in')) : ?>
-                      <li><a href="<?php echo base_url(); ?>users/logout">Logout</a></li>
-                    <?php endif; ?>
-                    -->
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
->>>>>>> a44ef185deca3a5ecb76fcef002125820e20107d
 
 <div class="background">
 	<head>
@@ -82,8 +41,4 @@
         </nav>
     </header> 
 	</head>
-<<<<<<< HEAD
 	<body> 
-=======
-	<body>
->>>>>>> a44ef185deca3a5ecb76fcef002125820e20107d
