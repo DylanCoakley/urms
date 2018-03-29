5d3f003a947bb76cbebb8f4b06206bd545cc1e5a
Dylan Coakley <34963733+DylanCoakley@users.noreply.github.com>
2018-03-29 10:41:18 -0300

    SQL Database Import v4

--- urms.sql
+++ urms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2018 at 08:10 PM
+-- Generation Time: Mar 29, 2018 at 03:39 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -31,18 +31,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `accounttype` (
   `RaffleID` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
-  `Role` varchar(255) NOT NULL DEFAULT 'User'
+  `Role` varchar(255) NOT NULL DEFAULT 'User',
+  `AllocatedTickets` int(11) NOT NULL DEFAULT '2147483647'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `accounttype`
 --
 
-INSERT INTO `accounttype` (`RaffleID`, `UserID`, `Role`) VALUES
-(1, 1, 'Administrator'),
-(2, 4, 'Administrator'),
-(3, 4, 'Administrator'),
-(4, 4, 'Administrator');
+INSERT INTO `accounttype` (`RaffleID`, `UserID`, `Role`, `AllocatedTickets`) VALUES
+(1, 1, 'Administrator', 2147483647),
+(2, 4, 'Administrator', 2147483647),
+(3, 4, 'Administrator', 2147483647),
+(4, 4, 'Administrator', 2147483647),
+(5, 9, 'Administrator', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -52,7 +54,7 @@ INSERT INTO `accounttype` (`RaffleID`, `UserID`, `Role`) VALUES
 
 CREATE TABLE `raffle` (
   `RaffleID` int(11) NOT NULL,
-  `Name` varchar(255) NOT NULL,
+  `RaffleName` varchar(255) NOT NULL,
   `Description` text NOT NULL,
   `StartDate` date NOT NULL,
   `EndDate` date NOT NULL,
@@ -63,11 +65,12 @@ CREATE TABLE `raffle` (
 -- Dumping data for table `raffle`
 --
 
-INSERT INTO `raffle` (`RaffleID`, `Name`, `Description`, `StartDate`, `EndDate`, `MaxTickets`) VALUES
+INSERT INTO `raffle` (`RaffleID`, `RaffleName`, `Description`, `StartDate`, `EndDate`, `MaxTickets`) VALUES
 (1, 'Man Lin Raffle', 'Chance to win an HTML website with turquoise coloring!', '2018-03-01', '2018-03-24', 255),
 (2, 'The Best Raffle', 'Chance to win a large elephant!				', '2014-01-01', '2015-01-01', 256),
 (3, 'Super Raffle', 'This is the next best thing since sliced bread				', '2014-02-02', '2016-02-02', 1000),
-(4, 'Another Cool Raffle', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa				', '2012-12-23', '2013-01-15', 2200);
+(4, 'Another Cool Raffle', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa				', '2012-12-23', '2013-01-15', 2200),
+(5, 'Raffle', 'ahsdijasbhdbuasdbuasjudbh', '2015-05-05', '5000-05-05', 255);
 
 -- --------------------------------------------------------
 
@@ -94,7 +97,15 @@ INSERT INTO `request` (`RequestID`, `UserID`, `RaffleID`, `Type`, `Notes`, `Quan
 (6, 1, 1, 'Join', '', 0, '2018-03-23 15:43:31', 0),
 (7, 1, 4, 'Join', '', 0, '2018-03-23 15:53:17', 0),
 (8, 4, 4, 'Join', '', 0, '2018-03-23 18:36:34', 0),
-(9, 4, 2, 'Join', '', 0, '2018-03-23 18:37:31', 0);
+(9, 4, 2, 'Join', '', 0, '2018-03-23 18:37:31', 0),
+(10, 4, 1, 'Join', '', 0, '2018-03-23 22:08:47', 0),
+(11, 9, 1, 'Ticket_Alloc', '', 2, '2018-03-28 13:49:25', 0),
+(12, 9, 1, 'Ticket_Alloc', '', 3, '2018-03-28 13:50:20', 0),
+(13, 9, 1, 'Ticket_Alloc', '', 3, '2018-03-28 16:26:54', 0),
+(14, 9, 1, 'Ticket_Alloc', '', 5, '2018-03-28 16:39:00', 0),
+(15, 9, 4, 'Join', '', 0, '2018-03-28 18:57:29', 0),
+(16, 9, 3, 'Join', '', 0, '2018-03-28 18:57:38', 0),
+(17, 9, 1, 'Join', '', 0, '2018-03-28 18:57:44', 0);
 
 -- --------------------------------------------------------
 
@@ -123,7 +134,8 @@ INSERT INTO `ticket` (`TicketID`, `RaffleID`, `UserID`, `Name`, `Address`, `Phon
 (2, 2, 1, 'Max Jennings', '123 Hall Street', '222-222-2222', 'max@max.com', 1, '2018-03-19'),
 (3, 1, 2, 'Sam Mckinnon', '123 Hall Crescent', '333-333-3333', 'sam@sam.com', 1, '2018-03-18'),
 (4, 1, 2, 'Max Jennings', '123 Hall Street', '222-222-2222', 'max@max.com', 1, '2018-03-19'),
-(5, 2, 2, 'Sam Mckinnon', '123 Hall Crescent', '333-333-3333', 'sam@sam.com', 1, '2018-03-18');
+(5, 2, 2, 'Sam Mckinnon', '123 Hall Crescent', '333-333-3333', 'sam@sam.com', 1, '2018-03-18'),
+(6, 1, 9, 'Bob', '1 Bob Street', '1234567890', 'bob@bob.com', 1, '2018-03-28');
 
 -- --------------------------------------------------------
 
@@ -133,9 +145,8 @@ INSERT INTO `ticket` (`TicketID`, `RaffleID`, `UserID`, `Name`, `Address`, `Phon
 
 CREATE TABLE `user` (
   `UserID` int(11) NOT NULL,
-  `Username` varchar(255) NOT NULL,
+  `UserName` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
-  `Name` varchar(255) NOT NULL,
   `Address` varchar(255) NOT NULL,
   `Phone` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL
@@ -145,13 +156,14 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`UserID`, `Username`, `Password`, `Name`, `Address`, `Phone`, `Email`) VALUES
-(1, 'dylan', 'd_coakes420', 'Dylan Coakley', '1 Infinity Loop', '444-444-4444', 'ilovecomputers@dylan.com'),
-(2, 'KatieCBLovr', 'katieroxs1997', 'Katie', 'MacEachern', '555-555-5555', '1015TheHawk1Fan@katie.com'),
-(4, '', 'ededed', 'Ed', 'Ed', 'Ed', 'ed@ed.com'),
-(5, '', 'csci485', 'Dylan Coakley', '63 MacIntyre Lane', '9025773557', 'x2014gvw@stfx.ca'),
-(6, '', 'computer', 'Dylan Coakley', '63 MacIntyre Lane', '9027369034', 'dylan_coakley11@hotmail.com'),
-(8, '', 'bababa', 'Buddy', 'B', '1234567890', 'buddy@bud.ca');
+INSERT INTO `user` (`UserID`, `UserName`, `Password`, `Address`, `Phone`, `Email`) VALUES
+(1, 'Dylan Coakley', 'd_coakes420', '1 Infinity Loop', '444-444-4444', 'ilovecomputers@dylan.com'),
+(2, 'Katie', 'katieroxs1997', 'MacEachern', '555-555-5555', '1015TheHawk1Fan@katie.com'),
+(4, 'Ed', 'ededed', 'Ed', 'Ed', 'ed@ed.com'),
+(5, 'Dylan Coakley', 'csci485', '63 MacIntyre Lane', '9025773557', 'x2014gvw@stfx.ca'),
+(6, 'Dylan Coakley', 'computer', '63 MacIntyre Lane', '9027369034', 'dylan_coakley11@hotmail.com'),
+(8, 'Buddy', 'bababa', 'B', '1234567890', 'buddy@bud.ca'),
+(9, 'bobbybob', '$2y$10$YTsRpO0U0UmOc6UTV73x0ezvj2xhXOTegzKwKYznemTMyNf24.dd.', 'bob', '908765435', '');
 
 --
 -- Indexes for dumped tables
@@ -195,25 +207,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `raffle`
 --
 ALTER TABLE `raffle`
-  MODIFY `RaffleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `RaffleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `request`
 --
 ALTER TABLE `request`
-  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `ticket`
 --
 ALTER TABLE `ticket`
-  MODIFY `TicketID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `TicketID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
