ee32a25f6b34bc149f2910a580eb0c9dede94d54
Ethan Rouse <35127302+Rouseew@users.noreply.github.com>
2018-03-30 07:09:50 -0300

    Added different graph to seller statistics page
    Code for previous graph included "Trial Version" at the base so I used a different graphing resource. Code for the previous graph is still available.

--- application/views/users/seller-statistics.php
+++ application/views/users/seller-statistics.php
@@ -1,11 +1,11 @@
-<h2><?= $title ?></h2>
+<!--<h2><?= $title ?></h2>
 
 <?php
  
-$dataPoints = array( 
-	array("label"=>"Tickets Sold","y"=>$tickets_sold),
-	array("label"=>"Tickets Remaining","y"=>$tickets_remaining) 
-)
+//$dataPoints = array( 
+	//array("label"=>"Tickets Sold","y"=>$tickets_sold),
+	//array("label"=>"Tickets Remaining","y"=>$tickets_remaining) 
+//)
  
 ?>
 <!DOCTYPE HTML>
@@ -31,10 +31,43 @@ var chart = new CanvasJS.Chart("chartContainer", {
 chart.render();
  
 }
+-->
+
+
+<h2><?= $title ?></h2>
 </script>
 </head>
 <body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<div id="chartContainer" style="height: 10px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
+
+<?php
+$dataPoints=[$tickets_sold, $tickets_remaining]; 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src = "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+</head>
+<body>
+	<canvas id="doughnut-chart" width="800" height="450"></canvas>
+	<script>
+		var dataArray = [<?php echo join(',',$dataPoints); ?>];
+		new Chart(document.getElementById("doughnut-chart"), {
+		    type: 'doughnut',
+		    data: {
+		        labels: ['Tickets Sold', 'Tickets Remaining'],
+		        datasets: [{
+		            label: 'Tickets',
+		            data: dataArray, // 3. use the javascript variable here.
+		            backgroundColor: ["#3e95cd", "#8e5ea2"]
+		        }]
+		    }
+		});
+	</script>
 </body>
 </html>
