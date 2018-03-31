51033e11b2973904942362fc77d023d6765b5862
DylanCoakley <dylan.coakley11@gmail.com>
2018-03-31 20:46:37 -0300

    Small change to TCPDF file

--- application/views/raffles/raffle-print-tickets.php
+++ application/views/raffles/raffle-print-tickets.php
@@ -29,10 +29,7 @@ $pdf = new Pdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', f
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
 $pdf->SetTitle('RaffleTickets');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
 // remove default header/footer
 $pdf->setPrintHeader(false);
@@ -92,94 +89,11 @@ foreach ($tickets as $ticket) {
 	$counter++;
 }
 
-// Multicell test
-/*
-$pdf->MultiCell(90, 5, $txt, 1, 'L', 0, 0, '', '', true);
-$pdf->MultiCell(90, 5, $txt, 1, 'L', 0, 1, '', '', true);
-$pdf->MultiCell(90, 5, '[CENTER] '.$txt, 1, 'L', 0, 0, '', '', true);
-$pdf->MultiCell(90, 5, '[RIGHT] '.$txt, 1, 'L', 0, 1, '', '', true);
-*/
-/*
-
-$pdf->MultiCell(55, 5, '[JUSTIFY] '.$txt."\n", 1, 'J', 1, 2, '' ,'', true);
-$pdf->MultiCell(55, 5, '[DEFAULT] '.$txt, 1, '', 0, 1, '', '', true);
-
-$pdf->Ln(4);
-
-// set color for background
-$pdf->SetFillColor(220, 255, 220);
-
-// Vertical alignment
-$pdf->MultiCell(55, 40, '[VERTICAL ALIGNMENT - TOP] '.$txt, 1, 'J', 1, 0, '', '', true, 0, false, true, 40, 'T');
-$pdf->MultiCell(55, 40, '[VERTICAL ALIGNMENT - MIDDLE] '.$txt, 1, 'J', 1, 0, '', '', true, 0, false, true, 40, 'M');
-$pdf->MultiCell(55, 40, '[VERTICAL ALIGNMENT - BOTTOM] '.$txt, 1, 'J', 1, 1, '', '', true, 0, false, true, 40, 'B');
-
-$pdf->Ln(4);
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// set color for background
-$pdf->SetFillColor(215, 235, 255);
-
-// set some text for example
-$txt = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed imperdiet lectus. Phasellus quis velit velit, non condimentum quam. Sed neque urna, ultrices ac volutpat vel, laoreet vitae augue. Sed vel velit erat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras eget velit nulla, eu sagittis elit. Nunc ac arcu est, in lobortis tellus. Praesent condimentum rhoncus sodales. In hac habitasse platea dictumst. Proin porta eros pharetra enim tincidunt dignissim nec vel dolor. Cras sapien elit, ornare ac dignissim eu, ultricies ac eros. Maecenas augue magna, ultrices a congue in, mollis eu nulla. Nunc venenatis massa at est eleifend faucibus. Vivamus sed risus lectus, nec interdum nunc.
-
-Fusce et felis vitae diam lobortis sollicitudin. Aenean tincidunt accumsan nisi, id vehicula quam laoreet elementum. Phasellus egestas interdum erat, et viverra ipsum ultricies ac. Praesent sagittis augue at augue volutpat eleifend. Cras nec orci neque. Mauris bibendum posuere blandit. Donec feugiat mollis dui sit amet pellentesque. Sed a enim justo. Donec tincidunt, nisl eget elementum aliquam, odio ipsum ultrices quam, eu porttitor ligula urna at lorem. Donec varius, eros et convallis laoreet, ligula tellus consequat felis, ut ornare metus tellus sodales velit. Duis sed diam ante. Ut rutrum malesuada massa, vitae consectetur ipsum rhoncus sed. Suspendisse potenti. Pellentesque a congue massa.';
-
-// print a blox of text using multicell()
-$pdf->MultiCell(80, 5, $txt."\n", 1, 'J', 1, 1, '' ,'', true);
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// AUTO-FITTING
-
-// set color for background
-$pdf->SetFillColor(255, 235, 235);
-
-// Fit text on cell by reducing font size
-$pdf->MultiCell(55, 60, '[FIT CELL] '.$txt."\n", 1, 'J', 1, 1, 125, 145, true, 0, false, true, 60, 'M', true);
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-// CUSTOM PADDING
-
-// set color for background
-$pdf->SetFillColor(255, 255, 215);
-
-// set font
-$pdf->SetFont('helvetica', '', 8);
-
-// set cell padding
-$pdf->setCellPaddings(2, 4, 6, 8);
-
-$txt = "CUSTOM PADDING:\nLeft=2, Top=4, Right=6, Bottom=8\nLorem ipsum dolor sit amet, consectetur adipiscing elit. In sed imperdiet lectus. Phasellus quis velit velit, non condimentum quam. Sed neque urna, ultrices ac volutpat vel, laoreet vitae augue.\n";
-
-$pdf->MultiCell(55, 5, $txt, 1, 'J', 1, 2, 125, 210, true);
-*/
 // move pointer to last page
 $pdf->lastPage();
 
-// ---------------------------------------------------------
 
 //Close and output PDF document
-$pdf->Output('example_005.pdf', 'I');
+$pdf->Output('RaffleTickets.pdf', 'I');
 
-//============================================================+
-// END OF FILE
-//============================================================+
-/*
-	$pdf = new Pdf('P', 'mm', 'A4', true, 'UTF-8', false);
-	$pdf->SetTitle('My Title');
-	$pdf->SetHeaderMargin(30);
-	$pdf->SetTopMargin(20);
-	$pdf->setFooterMargin(20);
-	$pdf->SetAutoPageBreak(true);
-	$pdf->SetAuthor('Author');
-	$pdf->SetDisplayMode('real', 'default');
-
-	$pdf->AddPage();
-
-	$pdf->Write(5, 'Some sample text');
-	$pdf->Output('My-File-Name.pdf', 'I');
-	*/
 ?>
