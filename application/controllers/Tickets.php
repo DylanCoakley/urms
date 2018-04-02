ddaa5179545acf1d1fdb1842055d92871ff8c9ee
Ethan Rouse <35127302+Rouseew@users.noreply.github.com>
2018-04-02 18:05:08 -0300

    Added MoneyRaised Updating

--- application/controllers/Tickets.php
+++ application/controllers/Tickets.php
@@ -1,12 +1,14 @@
 <?php 
 	class Tickets extends CI_Controller {
+
 		public function sell_tickets() {//sell($raffle_id) {
 			// First check if logged in
 			if(!$this->session->userdata('logged_in')) {
 				redirect('users/login');
 			}
-			
+
 			$data['title'] = 'Sell Tickets';
+
 			$this->form_validation->set_rules('ticket_quantity', 'Ticket Quantity', 'required');
 			$this->form_validation->set_rules('name', 'Customer Name', 'required');
 			$this->form_validation->set_rules('phone', 'Customer Phone', 'required');
@@ -18,18 +20,22 @@
 				$this->load->view('tickets/sell-tickets', $data);
 				$this->load->view('templates/footer');
 			} else {
-
 				$ticket_quantity = $this->input->post('ticket_quantity');
+
 				$book_quantity = floor($ticket_quantity / 3);
+
 				$indiv_tickets = $ticket_quantity % 3;
+
+				$price = ($indiv_tickets * 50) + ($book_quantity * 100);
+
 				$user_id = $this->session->userdata('user_id');
 
-				if(!$this->ticket_model->update_tickets($ticket_quantity, $user_id))
+				if(!$this->ticket_model->update_tickets($ticket_quantity, $user_id, $price))
 				{
 					$this->session->set_flashdata('invalid_sale', 'Sale of ' . $total_tickets . ' tickets failed! Insufficient tickets remaining!');
+
 					redirect('tickets/sell_tickets');
 				} else {
-
 					$this->session->set_flashdata('ticket_sale', 'Sale of ' . $indiv_tickets . ' ticket(s) and ' . $book_quantity .' book(s) to ' . $this->input->post('name') . ' successful!');
 
 			    	redirect('tickets/sell_tickets');
